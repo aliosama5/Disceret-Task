568c217823d16ce35af827a189b908fd9411d0dc
aliosama5 <121612915+aliosama5@users.noreply.github.com>
2022-12-29 13:25:03 +0200

    Add files via upload

---
+++ Disceret task1/Disceret task1/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;  
+public class Disceretproject1
+{
+    public static void Main()
+    {
+        int num, i, ctr, stnumber, endnumber;
+        Console.WriteLine("Disceret Task: project1");
+        Console.Write("Input starting number of range: ");
+        stnumber = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Input ending number of range : ");
+        endnumber = Convert.ToInt32(Console.ReadLine());
+        Console.Write("The prime numbers between {0} and {1} are : \n", stnumber, endnumber);
+
+        for (num = stnumber; num <= endnumber; num++)
+        {
+            ctr = 0;
+
+            for (i = 2; i <= num / 2; i++)
+            {
+                if (num % i == 0)
+                {
+                    ctr++;
+                    break;
+                }
+            }
+
+            if (ctr == 0 && num != 1)
+                Console.Write("{0} ", num);
+        }
+        Console.Write("\n");
+    }
+}
---
+++ Disceret task2/Disceret task2/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+public class Disceretproject2
+{
+    public static void Main()
+    {
+        int num, i, sum, stanumber, endnumber;
+        Console.WriteLine("Disceret Task: project2");
+        Console.Write("Input the starting range or number : ");
+        stanumber = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Input the ending range of number : ");
+        endnumber = Convert.ToInt32(Console.ReadLine());
+        Console.Write("The Perfect numbers within the given range : ");
+        for (num = stanumber; num <= endnumber; num++)
+        {
+            i = 1;
+            sum = 0;
+            while (i < num)
+            {
+                if (num % i == 0)
+                    sum = sum + i;
+                i++;
+            }
+            if (sum == num)
+                Console.Write("{0} ", num);
+        }
+        Console.Write("\n");
+    }
+}
